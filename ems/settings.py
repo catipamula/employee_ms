0eec7cff0e729c48b213ce0382425bf48cc23fcb
Chakravarthi Atipamula <119150797+catipamula@users.noreply.github.com>
2026-06-30 12:47:40 +0530

    Refactor static file settings in settings.py

--- ems/settings.py
+++ ems/settings.py
@@ -125,13 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = (
+    './static',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
